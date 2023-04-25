a074be982e72dc1bdc6798330033e70e9c043764
Almedin Alikadic <almedinalikadic158@gmail.com>
2023-04-25 11:30:44 +0200

    Seperated SetBrowserState from BeforeTestRun

--- Support/Hooks.cs
+++ Support/Hooks.cs
@@ -30,25 +30,14 @@ namespace PSF.Support
             _scenarioContext = scenarioContext;
         }
 
-        //Use both BeforeTestRun and BeforeScenario in case there are situation where you would like to skip logging in, if there is no situation, just use a basic BeforeScenario without importing the state.json
-        //BeforeTestRun should perform the login, after that the state is saved inside of the state.json
         [BeforeTestRun]
         public static async Task BeforeTestRun()
         {
-            PlaywrightContext = await Playwright.CreateAsync();
+            //Add extent report here
 
-            Browser = await InitializeBrowser(Browsers.Chromium.ToString());
 
-            BrowserContext = await Browser.NewContextAsync(new BrowserNewContextOptions
-            {
-                ViewportSize = ViewportSize.NoViewport
-            });
-
-            var page = await BrowserContext.NewPageAsync();
-
-            await SetBrowserState(page);
-
-            await page.CloseAsync();
+            //To skip loggin in each test, you can simply call SetBrowserState and perform the login inside of the function
+            await SetBrowserState();
         }
 
         [BeforeScenario]
@@ -58,6 +47,7 @@ namespace PSF.Support
             //Initialise a browser - 'Chromium' can be changed to 'Firefox' or 'Webkit'
             Browser = await InitializeBrowser(Browsers.Chromium.ToString());
 
+            //This will cause an error if SetBrowserState isn't performed and a state.json file does not exist
             BrowserContext = await GetBrowserState();
 
             Page = await BrowserContext.NewPageAsync();
@@ -106,8 +96,20 @@ namespace PSF.Support
             });
         }
 
-        public static async Task SetBrowserState(IPage page)
+        public static async Task SetBrowserState()
         {
+            PlaywrightContext = await Playwright.CreateAsync();
+
+            Browser = await InitializeBrowser(Browsers.Chromium.ToString());
+
+            BrowserContext = await Browser.NewContextAsync(new BrowserNewContextOptions
+            {
+                ViewportSize = ViewportSize.NoViewport
+            });
+
+            var page = await BrowserContext.NewPageAsync();
+
+            await page.CloseAsync();
             //Perform the login here
             await page.GotoAsync("https://www.saucedemo.com/");
             await page.Locator("input[id='user-name']").FillAsync("standard_user");
@@ -119,6 +121,7 @@ namespace PSF.Support
                 Path = "state.json"
             });
         }
+
         public async Task<IBrowserContext> GetBrowserState()
         {
             //State is imported based on tags, at the moment, if a test scenario has any tags, the state will be imported, this can also be changed so a specific tag causes the import.
